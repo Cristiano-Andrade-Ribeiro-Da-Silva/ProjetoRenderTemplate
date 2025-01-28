10dc10eb36a8633ca9e5a08152dadabd28c97ffa
Cristiano <cristiano060608@gmail.com>
2025-01-28 11:27:02 -0300

    ADD: random de curiosidades de imagem e ramdom de cores de fundo do site

--- app.py
+++ app.py
@@ -5,4 +5,31 @@ app = Flask(__name__)
 
 # Aqui está todas as minhas rotas
 
-app.run();
+lista_cores_fundo = ["red",
+                     "blue",
+                     "green",
+                     "#00ff00",
+                     "#00ffff",
+                     "#555555"]
+
+lista_conquistas = [
+    "Através de substâncias químicas liberadas pelas raízes e folhas, as plantas podem se comunicar, alertando sobre pragas, enviando sinais de perigo ou até mesmo cooperando para a sobrevivência mútua.",
+                    
+    " As raízes das plantas desempenham um papel fundamental na busca por água e nutrientes. Algumas raízes podem se estender por distâncias incríveis e formar complexas redes subterrâneas.",
+
+    "Experimentos demonstraram que algumas plantas podem 'lembrar'de condições de estresse, como seca ou ataque de pragas, e desenvolver mecanismos de defesa mais eficientes em futuras situações semelhantes.",
+
+    "O solo é um ecossistema vibrante, repleto de microrganismos que formam relações simbióticas com as plantas. Bactérias fixadoras de nitrogênio, por exemplo, fornecem nutrientes essenciais para o crescimento das plantas."]
+
+@app.route("/sobre")
+def pagina_sobre():
+    cores = random.choice(lista_cores_fundo)
+    conquistas = random.choice(lista_conquistas)
+    return render_template("sobre.html", cores_html = cores,  conquistas_html = conquistas)
+
+
+
+
+
+
+app.run(debug= True)
