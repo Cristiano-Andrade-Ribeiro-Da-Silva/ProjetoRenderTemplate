caea629c82cc160f184056a94f7b6d5884e64dd4
Cristiano <cristiano060608@gmail.com>
2025-02-04 11:29:21 -0300

    ADD: muitas coisas

--- app.py
+++ app.py
@@ -5,27 +5,56 @@ app = Flask(__name__)
 
 # Aqui está todas as minhas rotas
 
-lista_cores_fundo = ["red",
-                     "blue",
-                     "green",
-                     "#00ff00",
-                     "#00ffff",
-                     "#555555"]
+lista_cores = ["#6481D8",
+                     "#BB64D8",
+                     "#65D963",
+                     "#D88164"]
 
-lista_conquistas = [
-    "Através de substâncias químicas liberadas pelas raízes e folhas, as plantas podem se comunicar, alertando sobre pragas, enviando sinais de perigo ou até mesmo cooperando para a sobrevivência mútua.",
+
+lista_curiosidades = [
+    "Os crocodilos são os parentes vivos mais próximos dos dinossauros, junto com as aves. Eles surgiram há cerca de 200 milhões de anos, na era Mesozoica, e sobreviveram à extinção em massa que dizimou os dinossauros não-aviários.",
                     
-    " As raízes das plantas desempenham um papel fundamental na busca por água e nutrientes. Algumas raízes podem se estender por distâncias incríveis e formar complexas redes subterrâneas.",
+    "Crocodilos são mestres da camuflagem, com sua pele escamosa e coloração que se confundem com o ambiente. Eles passam a maior parte do tempo na água, com apenas os olhos e narinas visíveis, espreitando suas presas.",
 
     "Experimentos demonstraram que algumas plantas podem 'lembrar'de condições de estresse, como seca ou ataque de pragas, e desenvolver mecanismos de defesa mais eficientes em futuras situações semelhantes.",
 
-    "O solo é um ecossistema vibrante, repleto de microrganismos que formam relações simbióticas com as plantas. Bactérias fixadoras de nitrogênio, por exemplo, fornecem nutrientes essenciais para o crescimento das plantas."]
+    "Crocodilos têm a capacidade de regenerar seus dentes ao longo da vida. Quando um dente cai ou se quebra, um novo dente cresce em seu lugar, garantindo que eles sempre tenham uma dentição afiada para caçar.",
+
+    "Crocodilos possuem excelente visão noturna, o que os torna predadores eficientes na escuridão. Seus olhos são adaptados para captar a luz fraca, permitindo que eles vejam suas presas mesmo em condições de pouca luminosidade.",
+
+    "Crocodilos possuem sensores de vibração em sua mandíbula, que os ajudam a detectar movimentos na água. Esses sensores são extremamente sensíveis e permitem que eles localizem suas presas mesmo quando estão submersas ou escondidas.",
+
+    "Crocodilos são animais de longa vida, podendo viver até 70 anos ou mais. Seu metabolismo lento e sua capacidade de se adaptar a diferentes ambientes contribuem para sua longevidade.",
+    
+    "Crocodilos se comunicam através de vocalizações, como grunhidos e sibilos. Esses sons são usados para demarcar território, atrair parceiros e alertar sobre perigos.",
+    
+    "Crocodilos fêmeas são mães dedicadas, construindo ninhos para seus ovos e protegendo seus filhotes após o nascimento. Elas carregam os filhotes em suas bocas para protegê-los de predadores e os ajudam a encontrar comida.",
+    
+    "Existem cerca de 15 espécies de crocodilos no mundo, que habitam rios, lagos e pântanos em diferentes continentes. Algumas espécies, como o crocodilo-de-água-salgada, são os maiores répteis do mundo, podendo atingir até 7 metros de comprimento."]
+
+
+lista_img = ["crocodilo-01.jpg","crocodilo-02.jpg","crocodilo-03.jpg", "crocodilo-04.jpg"]
+
+@app.route("/")
+def pagina_principal():
+    cores = random.choice(lista_cores)
+    curiosidade = random.choice(lista_curiosidades)
+    return render_template("principal.html", 
+                           cores_html = cores, 
+                           curiosidade_html = curiosidade)
+
 
 @app.route("/sobre")
 def pagina_sobre():
-    cores = random.choice(lista_cores_fundo)
-    conquistas = random.choice(lista_conquistas)
-    return render_template("sobre.html", cores_html = cores,  conquistas_html = conquistas)
+    cor = random.choice(lista_cores)
+    curiosidade = random.choice(lista_curiosidades)
+    img = random.choice(lista_img)
+    return render_template("sobre.html", cor_html = cor,  curiosidade_html = curiosidade, img_html = img)
+
+
+@app.route("/cadastro")
+def pagina_cadastro():
+    return render_template("cadastro.html", frases_html = lista_curiosidades)
 
 
 
