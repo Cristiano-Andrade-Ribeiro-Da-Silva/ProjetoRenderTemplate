1192ae04ff17f7ebd291919f09d75ea7e3ba1477
Cristiano <cristiano060608@gmail.com>
2025-02-18 10:55:32 -0300

    ADD: Jeitos de adcionar cores no fundo da rota "/sobre", pasta "data" adicionada para melhor organizção das listas e substituição de imagens.

--- app.py
+++ app.py
@@ -1,44 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+from .data import listas_configuracao as listas
 import random
 
 app = Flask(__name__)
 
 # Aqui está todas as minhas rotas
 
-lista_cores = ["#6481D8",
-                     "#BB64D8",
-                     "#65D963",
-                     "#D88164"]
-
-
-lista_curiosidades = [
-    "Os crocodilos são os parentes vivos mais próximos dos dinossauros, junto com as aves. Eles surgiram há cerca de 200 milhões de anos, na era Mesozoica, e sobreviveram à extinção em massa que dizimou os dinossauros não-aviários.",
-                    
-    "Crocodilos são mestres da camuflagem, com sua pele escamosa e coloração que se confundem com o ambiente. Eles passam a maior parte do tempo na água, com apenas os olhos e narinas visíveis, espreitando suas presas.",
-
-    "Experimentos demonstraram que algumas plantas podem 'lembrar'de condições de estresse, como seca ou ataque de pragas, e desenvolver mecanismos de defesa mais eficientes em futuras situações semelhantes.",
-
-    "Crocodilos têm a capacidade de regenerar seus dentes ao longo da vida. Quando um dente cai ou se quebra, um novo dente cresce em seu lugar, garantindo que eles sempre tenham uma dentição afiada para caçar.",
-
-    "Crocodilos possuem excelente visão noturna, o que os torna predadores eficientes na escuridão. Seus olhos são adaptados para captar a luz fraca, permitindo que eles vejam suas presas mesmo em condições de pouca luminosidade.",
-
-    "Crocodilos possuem sensores de vibração em sua mandíbula, que os ajudam a detectar movimentos na água. Esses sensores são extremamente sensíveis e permitem que eles localizem suas presas mesmo quando estão submersas ou escondidas.",
-
-    "Crocodilos são animais de longa vida, podendo viver até 70 anos ou mais. Seu metabolismo lento e sua capacidade de se adaptar a diferentes ambientes contribuem para sua longevidade.",
-    
-    "Crocodilos se comunicam através de vocalizações, como grunhidos e sibilos. Esses sons são usados para demarcar território, atrair parceiros e alertar sobre perigos.",
-    
-    "Crocodilos fêmeas são mães dedicadas, construindo ninhos para seus ovos e protegendo seus filhotes após o nascimento. Elas carregam os filhotes em suas bocas para protegê-los de predadores e os ajudam a encontrar comida.",
-    
-    "Existem cerca de 15 espécies de crocodilos no mundo, que habitam rios, lagos e pântanos em diferentes continentes. Algumas espécies, como o crocodilo-de-água-salgada, são os maiores répteis do mundo, podendo atingir até 7 metros de comprimento."]
-
-
-lista_img = ["crocodilo-01.jpg","crocodilo-02.jpg","crocodilo-03.jpg", "crocodilo-04.jpg"]
-
 @app.route("/")
 def pagina_principal():
-    cores = random.choice(lista_cores)
-    curiosidade = random.choice(lista_curiosidades)
+    cores = random.choice(listas.lista_cores)
+    curiosidade = random.choice(listas.lista_curiosidades)
     return render_template("principal.html", 
                            cores_html = cores, 
                            curiosidade_html = curiosidade)
@@ -46,19 +17,43 @@ def pagina_principal():
 
 @app.route("/sobre")
 def pagina_sobre():
-    cor = random.choice(lista_cores)
-    curiosidade = random.choice(lista_curiosidades)
-    img = random.choice(lista_img)
+    cor = random.choice(listas.lista_cores)
+    curiosidade = random.choice(listas.lista_curiosidades)
+    img = random.choice(listas.lista_img)
     return render_template("sobre.html", cor_html = cor,  curiosidade_html = curiosidade, img_html = img)
 
 
 @app.route("/cadastro")
 def pagina_cadastro():
-    return render_template("cadastro.html", frases_html = lista_curiosidades)
+    return render_template("cadastro.html", frases_html = listas.lista_curiosidades)
 
 
+@app.route("/post/cadastrarfrase", methods=["POST"])
+def post_cadastrarfrase():
+    frases_html = request.form.get("frase")
+    listas.lista_curiosidades.append(frases_html)
+    return redirect ("/cadastro")
 
 
+@app.route("/cadastrocores", methods=["GET"])
+def pag_cores():
+    return render_template("lista-cores.html", lista_cores_html = listas.lista_cadastro_cor)
+
+
+@app.route("/post/cadastrarcor", methods=["POST"])
+def post_cadastrarcor():
+    cor_html = request.form.get("cores")
+    listas.lista_cadastro_cor.append(cor_html)
+    return redirect("/cadastrocores")
+
+
+@app.route("/cores/delete/<indice_cor>", methods=["GET"])
+def delete_cores(indice_cor):
+    # converte o 'indece_cor' para um numero inteiro pois estava em string
+    indice_cor = int(indice_cor)
+    #exclui a cor da lista através do indice
+    listas.lista_cadastro_cor.pop(indice_cor)
+    return redirect("/cadastrocores")
 
 
 app.run(debug= True)
---
+++ data/listas_configuracao.py
@@ -0,0 +1,39 @@
+lista_cores = [
+               "#6481D8",
+               "#BB64D8",
+               "#65D963",
+               "#D88164"]
+
+
+lista_cadastro_cor = []
+
+
+lista_curiosidades = [
+    "Os crocodilos são os parentes vivos mais próximos dos dinossauros, junto com as aves. Eles surgiram há cerca de 200 milhões de anos, na era Mesozoica, e sobreviveram à extinção em massa que dizimou os dinossauros não-aviários.",
+                    
+    "Crocodilos são mestres da camuflagem, com sua pele escamosa e coloração que se confundem com o ambiente. Eles passam a maior parte do tempo na água, com apenas os olhos e narinas visíveis, espreitando suas presas.",
+
+    "Experimentos demonstraram que algumas plantas podem 'lembrar'de condições de estresse, como seca ou ataque de pragas, e desenvolver mecanismos de defesa mais eficientes em futuras situações semelhantes.",
+
+    "Crocodilos têm a capacidade de regenerar seus dentes ao longo da vida. Quando um dente cai ou se quebra, um novo dente cresce em seu lugar, garantindo que eles sempre tenham uma dentição afiada para caçar.",
+
+    "Crocodilos possuem excelente visão noturna, o que os torna predadores eficientes na escuridão. Seus olhos são adaptados para captar a luz fraca, permitindo que eles vejam suas presas mesmo em condições de pouca luminosidade.",
+
+    "Crocodilos possuem sensores de vibração em sua mandíbula, que os ajudam a detectar movimentos na água. Esses sensores são extremamente sensíveis e permitem que eles localizem suas presas mesmo quando estão submersas ou escondidas.",
+
+    "Crocodilos são animais de longa vida, podendo viver até 70 anos ou mais. Seu metabolismo lento e sua capacidade de se adaptar a diferentes ambientes contribuem para sua longevidade.",
+    
+    "Crocodilos se comunicam através de vocalizações, como grunhidos e sibilos. Esses sons são usados para demarcar território, atrair parceiros e alertar sobre perigos.",
+    
+    "Crocodilos fêmeas são mães dedicadas, construindo ninhos para seus ovos e protegendo seus filhotes após o nascimento. Elas carregam os filhotes em suas bocas para protegê-los de predadores e os ajudam a encontrar comida.",
+    
+    "Existem cerca de 15 espécies de crocodilos no mundo, que habitam rios, lagos e pântanos em diferentes continentes. Algumas espécies, como o crocodilo-de-água-salgada, são os maiores répteis do mundo, podendo atingir até 7 metros de comprimento."]
+
+
+lista_img = [
+             "crocodilo-01.jpg",
+             "crocodilo-02.jpg",
+             "crocodilo-03.jpg", 
+             "crocodilo-04.jpg"]
+
+
